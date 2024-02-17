f7dd5e24871552f22e7090150f6d15f022dd5d09
julio-silveira <julioqueiroz15@gmail.com>
2024-02-17 09:49:17 -0300

    chore: created prisma global module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PayablesModule } from './modules/payables/payables.module';
+import { PrismaGlobalModule } from './modules/prisma/prisma.global.module';
 
 @Module({
-  imports: [PayablesModule],
+  imports: [
+    // Global Modules
+    PrismaGlobalModule,
+    // Non-Global Modules
+    PayablesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
